11cd122a8c9b9e6d386d1bbfd54262fbcb243097
pkgalaxy19 <87329424+pkgalaxy19@users.noreply.github.com>
2024-03-27 01:49:35 +0530

    whole project completed(Leqarned all react concepts and intermediate Material UI)

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
+import Logo from '../assets/images/Logo.png';
+
+const Navbar = () => (
+  <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '122px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
+    <Link to="/">
+      <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0px 20px' }} />
+    </Link>
+    <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
+     
+    </Stack>
+      <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '0px solid #FF2625'}}><h2>Home</h2></Link>
+      <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}><h2>Exercises</h2></a>
+  </Stack>
+);
+
+export default Navbar;
